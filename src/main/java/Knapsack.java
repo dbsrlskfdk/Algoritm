390122353193756f518c0a5ba0ee7e1d06ff8593
황윤기 <dbsrlskfdk@naver.com>
2022-06-18 19:30:24 +0900

    Modify Knapsack
    Add Indentation Code

--- src/main/java/Knapsack.java
+++ src/main/java/Knapsack.java
@@ -2,23 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Knapsack {
+    static HashMap<String, Integer> dict = new HashMap<>();
+    static int total = 0;
+    static void push_Dict(String knapsack_cnt, int i, int W){
+        if (dict.get("Knapsack["+i +","+W+"]") != null){
+            dict.put("Knapsack["+i +","+W+"]",  dict.get("Knapsack["+i +","+W+"]") + 1);
+        } else{
+            dict.put("Knapsack["+i +","+W+"]", 1);
+        }
+    }
     static int knapsack(int i, int W, ArrayList<Integer[]> A, int indent) {
         if (i < 0){
-            String inde = "\t".repeat(indent);
+/*            String inde = "\t".repeat(indent);
             System.out.println(inde+"Knapsack["+i +","+W+"]");
+            push_Dict("Knapsack["+i +","+W+"]",i ,W);*/
             return 0;
         }
         if (A.get(i)[0] > W){
             String inde = "\t".repeat(indent);
             System.out.println(inde+"Knapsack["+i +","+W+"]");
+            push_Dict("Knapsack["+i +","+W+"]",i ,W);
+            total += 1;
             System.out.println(inde+"W : "+ W + ", " + "Weight : "+A.get(i)[0]);
             return knapsack(i-1, W, A, indent+1);
         }else{
             String inde = "\t".repeat(indent);
             System.out.println(inde+"Knapsack["+i +","+W+"]");
+            push_Dict("Knapsack["+i +","+W+"]",i ,W);
+            total += 1;
             System.out.println(inde+"W : "+ W + ", " + "Weight : "+A.get(i)[0]);
             return Math.max(knapsack(i - 1, W, A, indent+1), knapsack(i - 1, W - A.get(i)[0], A,indent+1) + A.get(i)[1]);
         }
@@ -46,6 +61,8 @@ public class Knapsack {
         }*/
 
         System.out.println(knapsack(N-1, K, A, indent));
+        System.out.println(dict);
+        System.out.println("total : "+ total);
     }
 
 
